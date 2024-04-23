5f8998e0774bdf59ffdf5dfdcb6fdbb8aecd62a5
Yusuke Shimizu <c0b230802f@edu.teu.ac.jp>
2024-04-23 15:09:01 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,7 @@ import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1000, 700
 DELTA = {  # 移動量辞書（押下キー：移動量タプル）
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
@@ -13,6 +13,18 @@ DELTA = {  # 移動量辞書（押下キー：移動量タプル）
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    こうかとんRect，または，爆弾Rectの画面内外判定用の関数
+    引数：こうかとんRect，または，爆弾Rect
+    戻り値：横方向判定結果，縦方向判定結果（True：画面内／False：画面外）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: 
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -21,6 +33,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    # ここから爆弾の設定
     bd_img = pg.Surface((20,20))
     bd_img.set_colorkey((0,0,0))
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
@@ -34,7 +47,7 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        # こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, v in DELTA.items():
@@ -42,9 +55,17 @@ def main():
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        # 爆弾の移動と表示
         bd_rct.move_ip(vx, vy) 
         screen.blit(bd_img, bd_rct)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:  # 横方向にはみ出てたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出てたら
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
