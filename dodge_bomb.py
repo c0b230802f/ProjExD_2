c1eb7045a54c3fb0b6bf85c5bf694b3c9a77b1a9
Yusuke Shimizu <c0b230802f@edu.teu.ac.jp>
2024-04-23 15:17:24 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -46,6 +46,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        if kk_rct.colliderect(bd_rct): #こうかとんと爆弾がぶつかったら
+            print("Game Over ")
+            return
         screen.blit(bg_img, [0, 0]) 
         # こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
